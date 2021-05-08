d225c8657a428051159d06385be6ef4482ca8b4d
taha khorasani <tahakhorasani77@gmail.com>
2021-05-08 21:21:12 +0430

    just main and class

---
+++ index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Node{
+char data;
+int frequency;
+bool state;
+Node* right;
+Node* left;
+Node* parent;
+
+
+ Node(){
+
+    data=0
+    frequency=0
+    state=0
+    right=0
+    left=0
+    parent=0
+ }
+}
+
+
+
+int main(){
+    char letter[]={'n','h','s','d','.','i','v','g','l','o','e','a','r','$','t','m','p'}
+    
+    int frequency[]={8,4,2,1,1,5,2,3,5,5,12,6,7,14,7,4,4}
+
+
+
+    return 0;
+}
