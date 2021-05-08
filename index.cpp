e52ce24566ad45716510708582eff7e187b338b7
taha khorasani <tahakhorasani77@gmail.com>
2021-05-08 22:13:30 +0430

    create Node

--- index.cpp
+++ index.cpp
@@ -19,17 +19,31 @@ Node* parent;
     parent=0
  }
 }
-Node *tail-left=0;
-Node *tail-right=0;
+
+Node *left=0;
+Node *right=0;
+Node *current=0;
 
 void linkNode(letter , frequency){
-if(tail-left==0 && tail-right==0){
-    tail-left=new Node();
-    tail-right=new Node();
-    tail-left->character=letter[0];
-    tail-right->character=letter[1];
-    tail-left->parent=new Node();
-    tail-right->parent=tail-left->parent;
+if(left==0 && right==0){
+    left=new Node();
+    right=new Node();
+    left->character=letter[0];
+    right->character=letter[1];
+    left->frequency=frequency[0];
+    right->frequency=frequency[1];
+    left->parent=new Node();
+    right->parent=left->parent;
+    parent->frequency=left-frequency+right->frequency;
+    current=parent;
+}
+
+else{
+
+ for(int i=2;i<letter.length;i++){
+
+if(current.frequency)
+ }   
 }
 
 }
